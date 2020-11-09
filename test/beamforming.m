93137ebbbbf6e670d8e520a692728e1cdc617c40
spoter368 <colton.baldridge@gmail.com>
2020-11-08 23:05:16 -0500

    Added uniform circular array and monopole with radials

--- test/beamforming.m
+++ test/beamforming.m
@@ -2,24 +2,29 @@ clear all;
 close all;
 
 %% Beamformer
-c = 3e8;        % signal propagation speed
+c = 299792458;        % signal propagation speed
 fc = 22e6;       % signal carrier frequency
 lambda = c/fc;  % wavelength
 
-thetaad = 30;           % look directions (degrees)
-thetaan = 70;           % interference direction (degrees)
+az_target = 0;     % target azimuth (degrees)
+az_null = 65;       % interference direction (degrees)
+ele_target = 0;    % target elevation (degrees)
+ele_null = 45;      % null elevation (degrees)
 
-antenna = monopole('GroundPlaneLength', 43, 'GroundPlaneWidth', 43, 'Height', 43, 'Width', 0.25)
-ula = phased.ULA('NumElements',8,'Element', antenna, 'ElementSpacing', lambda/2);
+antenna = monopoleRadial('Height',lambda/4,'RadialLength',lambda/4,'Width',.1,'RadialWidth',.1,'NumRadials',4);
+%antenna = monopole('GroundPlaneLength', 43, 'GroundPlaneWidth', 43, 'Height', lambda/4, 'Width', 0.1)
+%array = phased.ULA('NumElements',8,'Element', antenna, 'ElementSpacing', lambda/2);
+array = phased.UCA('NumElements',8,'Element', antenna, 'Radius', lambda/2);
 
 % Generalized sidelobe canceller
 % Calculate the steering vector for null directions
-wn = steervec(getElementPosition(ula)/lambda,thetaan);
+steervec = phased.SteeringVector('SensorArray',array);
+wn = steervec(fc, [az_null; ele_null]);
 
 % Calculate the steering vectors for lookout directions
-wd = steervec(getElementPosition(ula)/lambda,thetaad);
-win = kaiser(8,4);
-wd = win.*wd;
+wd = steervec(fc, [az_target; ele_target]);
+%win = kaiser(8,4);
+%wd = win.*wd;
 
 % Compute the response of desired steering at null direction
 rn = wn'*wd/(wn'*wn);
@@ -27,41 +32,43 @@ rn = wn'*wd/(wn'*wn);
 % Sidelobe canceler - remove the response at null direction
 w = wd-wn*rn;
 
-% Plot the pattern
+% Plot azimuth cut 2D
 figure
-pattern(ula,fc,-180:0.2:180,0,'PropagationSpeed',c,'Type','powerdb',...
+pattern(array,fc,-180:0.2:180,0,'PropagationSpeed',c,'Type','powerdb',...
     'CoordinateSystem','rectangular','Weights',w);
 hold on; legend off;
-for i = 1:length(thetaan)
-    plot([thetaan(i) thetaan(i)],[-100 100],'r--','LineWidth',2)
+for i = 1:length(az_null)
+    plot([az_null(i) az_null(i)],[-100 100],'r--','LineWidth',2)
 end
-for z = 1:length(thetaad)
-    plot([thetaad(z) thetaad(z)],[-100 100],'g--','LineWidth',2)
+for z = 1:length(az_target)
+    plot([az_target(z) az_target(z)],[-100 100],'g--','LineWidth',2)
 end
 hold off;
 
-% Zoom
+% Zoom on null 2D
 figure
-pattern(ula,fc,-180:180,0,'PropagationSpeed',c,'Type','powerdb',...
+pattern(array,fc,-180:0.2:180,45,'PropagationSpeed',c,'Type','powerdb',...
     'CoordinateSystem','rectangular','Weights',w);
 hold on; legend off;
-for a = 1:length(thetaan)
-    plot([thetaan(a) thetaan(a)],[-100 100],'r--','LineWidth',2)
+for a = 1:length(az_null)
+    plot([az_null(a) az_null(a)],[-100 100],'r--','LineWidth',2)
 end
-for b = 1:length(thetaad)
-    plot([thetaad(b) thetaad(b)],[-100 100],'g--','LineWidth',2)
+for b = 1:length(az_target)
+    plot([az_target(b) az_target(b)],[-100 100],'g--','LineWidth',2)
 end
-xlim([thetaan-10 thetaan+10])
-legend(arrayfun(@(k)sprintf('%d degrees',k),thetaad,...
+xlim([az_null-10 az_null+10])
+legend(arrayfun(@(k)sprintf('%d degrees',k),az_target,...
     'UniformOutput',false),'Location','SouthEast');
 
 % 3-D
 figure
 rotate3d on
-pattern(ula,fc,'CoordinateSystem','polar','Type','powerdb',...
+pattern(array,fc,'CoordinateSystem','polar','Type','powerdb',...
         'PropagationSpeed',c,'Weights',w);
-view(50,20);
-ax = gca;
+view([45 45]);
+%ax = gca;
 %ax.Position = [-0.15 0.1 0.9 0.8];
-camva(4.5);
-campos([1000 -500 500]);
+%camva(4.5);
+%campos([1000 -500 500]);
+
+%viewArray(array)
