effbcd971d6dd7d6f7ce5deae76fa9e3640f7396
Chris Tousignant <ctous@vt.edu>
2020-12-06 14:36:11 -0500

    better plotting

--- test/great_circle.m
+++ test/great_circle.m
@@ -9,26 +9,31 @@ close all
 
 % Map
 figure
-ax = axesm('MapProjection','mercator','MapLatLimit',[-75 80],'Grid','on','Frame', 'on');
+ax = axesm('MapProjection','mercator',...
+           'MapLatLimit',[-75 80], 'MapLonLimit', [90 300], 'Grid','on','Frame', 'on');
 setm(ax, 'meridianlabel', 'on', 'parallellabel', 'on')
 land = shaperead('landareas', 'UseGeoCoords', true);
 geoshow(ax, land, 'FaceColor', [0.5 0.7 0.5])
+tightmap;
 
 % Select Current Location
 [current_lat, current_lon] = inputm(1);
+plotm(current_lat, current_lon, 'bo');
 
 % Select Peak
 [target_lat, target_lon] = inputm(1);
+plotm(target_lat, target_lon, 'go');
+linem([current_lat; target_lat], [current_lon; target_lon], 'g-');
 
 % Select Nulls
 [null_lat, null_lon] = inputm(1);
+plotm(null_lat, null_lon, 'ro');
+linem([current_lat; null_lat], [current_lon; null_lon], 'r-');
 
 % Determine Azimuth
 target_az = azimuth(current_lat, current_lon, target_lat, target_lon);
 null_az = azimuth(current_lat, current_lon, null_lat, null_lon);
 
 % Plot points
-linem([current_lat; target_lat], [current_lon; target_lon], 'g-*');
-linem([current_lat; null_lat], [current_lon; null_lon], 'r-*');
 
 
