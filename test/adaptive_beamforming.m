da39f3061b16ef3ef3f82d9a2e8a60fd206f3992
Chris Tousignant <ctous@vt.edu>
2021-01-30 16:40:27 -0500

    Null Placement expansion logic for generalized enemy azimuth inputs

--- test/adaptive_beamforming.m
+++ test/adaptive_beamforming.m
@@ -1,37 +1,60 @@
+% Multiple Wide Null Adaptive Beamformer
+% Date: 1/27/2021
 clear all;
 close all;
 
-%% Multiple Wide Null Adaptive Beamformer
-% Inputs
-N = 8;                            % Number of Elements
-az_target = 20;                   % Target Azimuth
-az_nulls = [-20 -25 -30 -40 -45]; % Null Azimuths -- TODO
-fc = 22e6;                        % Carrier frequency
+%% Inputs
+N = 8;                % Number of Elements
+azAlly = 30;          % Target Azimuth in degrees
+azEnemy = [-40 -10];  % Null Azimuths
+fc = 22e6;            % Carrier frequency
+nullSpacing = 5;      % Null spacing in degrees
 
-% Constants
+%% Constants
 c = physconst('LightSpeed'); % signal propagation speed
 lambda = c/fc;               % wavelength
+reservedDOF = [1 1 1 2 2 2]; % Reserved Degrees of Freedom based on N [3 4 ... 8]
 
-% Array Creation
+%% Globally Scoped Defines
+availableNulls = 0;
+requiredNulls = 0;
+nullsPerEnemyAzimuth = 0;
+wideEnemyNulls = [];
+steeringMatrix = [];
+desiredResponse = [];
+antennaWeights = [];
+
+%% Physical Array
 antenna = monopole('GroundPlaneLength', 43, 'GroundPlaneWidth', 43, 'Height', lambda/4, 'Width', 0.1);
 array = phased.ULA('NumElements',N,'Element', antenna, 'ElementSpacing', lambda/2);
 
-% TODO
-% Calculate locations for additional nulls
-az_nulls_expanded = az_nulls;
+%% Null Widening/Placement
+% Determine number of available nulls to place, leaving some free
+availableNulls = N - 1 - reservedDOF(N-2);
+requiredNulls = length(azEnemy);
+if availableNulls < requiredNulls
+    error("Error: With %i antennas, only %i nulls available. Requested %i nulls", N, availableNulls, requiredNulls);
+end
 
+% Determine how many nulls to assign to each requested enemy azimuth
+nullsPerEnemyAzimuth = availableNulls / requiredNulls;
+
+% Place all enemy nulls
+wideEnemyNulls = placeNulls(azEnemy, nullsPerEnemyAzimuth, nullSpacing)
+
+%% Antenna Weight Calculations
 % Steering Matrix
-A = steervec(getElementPosition(array)/lambda, [az_target az_nulls_expanded]);
+steeringMatrix = steervec(getElementPosition(array)/lambda, [azAlly wideEnemyNulls]);
 
 % Desired Response
-r = [1 zeros(1, length(az_nulls_expanded))];
+desiredResponse = [1 zeros(1, length(wideEnemyNulls))];
 
 % If we have a non-singular square matrix (N-1 nulls)
 % Honestly, we do not want to do this because we waste all of our
 % degrees of freedom placing nulls, leading to a performance loss
 % in the main beam
-if ~diff(size(A))
-    w = (r*inv(A))'; % Array Weights
+if ~diff(size(steeringMatrix))
+    antennaWeights = (desiredResponse*inv(steeringMatrix))'; % Array Weights
     
 % If singular matrix (less than N-1 nulls)
 % so that a solution can still be found.
@@ -39,34 +62,35 @@ if ~diff(size(A))
 % allow for extra unused degrees of freedom so that the main lobe doesn't
 % suffer any performance degredation (apart from widening due to scanning)
 else
-    w = (r*pinv(A))'; % Array Weights
+    antennaWeights = (desiredResponse*pinv(steeringMatrix))'; % Array Weights
 end
 
+%% Plotting
 % Plot 2D Rectangular azimuth cut
 figure
-pattern(array,fc,-180:1:180,0,'PropagationSpeed',c,'Type','powerdb',...
-    'CoordinateSystem','rectangular','Weights',w);
+pattern(array,fc,-180:.1:180,0,'PropagationSpeed',c,'Type','powerdb',...
+    'CoordinateSystem','rectangular','Weights',antennaWeights);
 xlim([-90 90]);
 hold on; legend off;
-for i = 1:length(az_nulls_expanded)
-    plot([az_nulls_expanded(i) az_nulls_expanded(i)],[-500 100],'r--','LineWidth',1)
+for i = 1:length(wideEnemyNulls)
+    plot([wideEnemyNulls(i) wideEnemyNulls(i)],[-500 100],'r--','LineWidth',1)
 end
-for z = 1:length(az_target)
-    plot([az_target(z) az_target(z)],[-500 100],'g--','LineWidth',1)
+for z = 1:length(azAlly)
+    plot([azAlly(z) azAlly(z)],[-500 100],'g--','LineWidth',1)
 end
 hold off;
 
 % Plot 2D Polar azimuth cut
 figure
-pattern(array,fc,-180:1:180,0,'PropagationSpeed',c,'Type','powerdb',...
-    'CoordinateSystem','polar','Weights',w);
+pattern(array,fc,-180:.1:180,0,'PropagationSpeed',c,'Type','powerdb',...
+    'CoordinateSystem','polar','Weights',antennaWeights);
 hold on; legend off;
 % TODO - get these lines plotting
-for i = 1:length(az_nulls_expanded)
-    plot([az_nulls_expanded(i) az_nulls_expanded(i)],[-500 100],'r--','LineWidth',1)
+for i = 1:length(wideEnemyNulls)
+    plot([wideEnemyNulls(i) wideEnemyNulls(i)],[-500 100],'r--','LineWidth',1)
 end
-for z = 1:length(az_target)
-    plot([az_target(z) az_target(z)],[-500 100],'g--','LineWidth',1)
+for z = 1:length(azAlly)
+    plot([azAlly(z) azAlly(z)],[-500 100],'g--','LineWidth',1)
 end
 hold off;
 
@@ -74,7 +98,7 @@ hold off;
 figure
 rotate3d on
 pattern(array,fc,-180:0.5:180,-90:.5:90,'CoordinateSystem','polar','Type','powerdb',...
-        'PropagationSpeed',c,'Weights',w);
+        'PropagationSpeed',c,'Weights',antennaWeights);
 view([45 45]);
 
 
---
+++ test/placeNulls.m
@@ -0,0 +1,43 @@
+function wideEnemyNulls = placeNulls(azEnemy, nullsPerEnemyAzimuth, nullSpacing)
+% Default
+wideEnemyNulls = [];
+
+% If the nulls can be divided up evenly
+if rem(nullsPerEnemyAzimuth, 1) == 0
+    disp("The nulls will be split evenly between all enemies");
+    % If there is an even number of nulls per enemy azimuth
+    if rem(nullsPerEnemyAzimuth, 2) == 0
+        disp("There is an even number of nulls per enemy");
+        for i=1:length(azEnemy)
+            azLow = azEnemy(i) - (nullSpacing * (nullsPerEnemyAzimuth/2 - 0.5));
+            azHigh = azEnemy(i) + (nullSpacing * (nullsPerEnemyAzimuth/2 - 0.5));
+            wideEnemyNulls = [wideEnemyNulls linspace(azLow, azHigh, nullsPerEnemyAzimuth)];
+        end
+    % If there are an odd number of nulls per enemy azimuth
+    else
+        disp("There is an odd number of nulls per enemy");
+        for i=1:length(azEnemy)
+            azLow = azEnemy(i) - (nullSpacing * floor(nullsPerEnemyAzimuth/2));
+            azHigh = azEnemy(i) + (nullSpacing * floor(nullsPerEnemyAzimuth/2));
+            wideEnemyNulls = [wideEnemyNulls linspace(azLow, azHigh, nullsPerEnemyAzimuth)];
+        end
+    end
+% If the nulls cannot be divided up evenly
+else
+    disp("The number of nulls is odd and therefore will not be placed evenly");
+    % Generate a list of how many nulls will be applied to each enemy azimuth
+    numNulls = ones(1, length(azEnemy)) * floor(nullsPerEnemyAzimuth);
+    remainingNulls = length(azEnemy) * (nullsPerEnemyAzimuth - floor(nullsPerEnemyAzimuth));
+    for j=1:remainingNulls
+        numNulls(j) = numNulls(j) + 1;
+    end
+    numNulls;
+    % Make a recursive call to determine null placement
+    for i=1:length(azEnemy)
+        wideEnemyNulls = [wideEnemyNulls placeNulls(azEnemy(i), numNulls(i), nullSpacing)];
+    end
+end
+
+wideEnemyNulls = wrapTo180(wideEnemyNulls);
+end
+
