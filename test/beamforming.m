c94d9a21ebaf6d8099ac8782aeb762ac9500b22b
spoter368 <colton.baldridge@gmail.com>
2021-03-01 20:08:41 -0500

    Added bandwidth viz to beamforming.m

--- test/beamforming.m
+++ test/beamforming.m
@@ -6,15 +6,15 @@ c = 299792458;        % signal propagation speed
 fc = 22e6;       % signal carrier frequency
 lambda = c/fc;  % wavelength
 
-az_target = 90;     % target azimuth (degrees)
-az_null = 0;       % interference direction (degrees)
+az_target = 110;     % target azimuth (degrees)
+az_null = 80;       % interference direction (degrees)
 ele_target = 0;    % target elevation (degrees)
 ele_null = 0;      % null elevation (degrees)
 
 antenna = monopoleRadial('Height',lambda/4,'RadialLength',lambda/4,'Width',.1,'RadialWidth',.1,'NumRadials',4);
 %antenna = monopole('GroundPlaneLength', 43, 'GroundPlaneWidth', 43, 'Height', lambda/4, 'Width', 0.1)
-array = phased.ULA('NumElements',8,'Element', antenna, 'ElementSpacing', lambda/2);
-%array = phased.UCA('NumElements',8,'Element', antenna, 'Radius', lambda/2);
+%array = phased.ULA('NumElements',8,'Element', antenna, 'ElementSpacing', lambda/2);
+array = phased.UCA('NumElements',8,'Element', antenna, 'Radius', lambda/2);
 
 % Generalized sidelobe canceller
 % Calculate the steering vector for null directions
@@ -60,12 +60,77 @@ xlim([min(az_null-10) max(az_null+10)])
 legend(arrayfun(@(k)sprintf('%d degrees',k),az_target,...
     'UniformOutput',false),'Location','SouthEast');
 
+%resolution in degrees
+patternRes = 3;
+
 % 3-D
 figure
 rotate3d on
-pattern(array,fc,-180:0.5:180,-90:.5:90,'CoordinateSystem','polar','Type','powerdb',...
+pattern(array,fc,-180:patternRes:180,-90:patternRes:90,'CoordinateSystem','polar','Type','powerdb',...
         'PropagationSpeed',c,'Weights',w);
-view([45 45]);
+title('Response at Carrier');
+
+% % +1.5kHz
+% %figure
+% rotate3d on
+% [pluspat, ~, ~] = pattern(array,(fc+1.5e3),-180:patternRes:180,-90:patternRes:90,'CoordinateSystem','polar','Type','powerdb',...
+%         'PropagationSpeed',c,'Weights',w);
+% %title('Response at +1.5kHz')
+% 
+% % -1.5kHz
+% %figure
+% rotate3d on
+% [minuspat, azi, elev] = pattern(array,(fc-1.5e3),-180:patternRes:180,-90:patternRes:90,'CoordinateSystem','polar','Type','powerdb',...
+%         'PropagationSpeed',c,'Weights',w);
+% %title('Response at -1.5kHz')
+% 
+% minusdiff = defpat - minuspat;
+% plusdiff = defpat - pluspat;
+% 
+% zrange = ([-5 5]);
+% 
+% figure
+% mesh(azi, elev, plusdiff);
+% ylabel('Elevation Angle (deg)')
+% xlabel('Azimuth Angle (deg)')
+% zlabel('Difference from Carrier Pattern (dB)')
+% title('+1.5kHz Difference From Carrier Pattern')
+% zlim(zrange);
+% caxis(zrange);
+% 
+% figure
+% mesh(azi, elev, minusdiff);
+% ylabel('Elevation Angle (deg)')
+% xlabel('Azimuth Angle (deg)')
+% zlabel('Difference from Carrier Pattern (dB)')
+% title('-1.5kHz Difference From Carrier Pattern')
+% zlim(zrange);
+% caxis(zrange);
+
+bw = 1e6;
+step = 1000;
+
+figure
+parallelResults = zeros(361,(bw/step)+1);
+parfor i = 0:(bw/step)
+    [parallelResults(:,i+1), ~, ~] = pattern(array,(fc-(bw/2)+(i*step)),-180:1:180,0,'PropagationSpeed',c,'Type','powerdb','CoordinateSystem','polar','Normalize',false,'Weights',w);
+end
+
+[~, plot_azimuth, ~] = pattern(array,(fc),-180:1:180,0,'PropagationSpeed',c,'Type','powerdb','CoordinateSystem','polar','Normalize',false,'Weights',w);
+zrange = ([-75 15]);
+plot_freq = fc-(bw/2):step:fc+(bw/2);
+plot_freq = plot_freq./1e+6;
+surf(plot_freq, plot_azimuth, parallelResults);
+shading interp;
+view(0,90);
+c = colorbar;
+c.Label.String = 'Gain (dBi)';
+caxis(zrange);
+ylabel('Azimuth Angle (deg)');
+xlabel('Frequency (MHz)')
+title('Affect of Transmission Bandwidth on Pattern (Elev. = 0 deg)');
+
+
 % phi = az';
 % theta = (90-el);
 % MagE = efield';
