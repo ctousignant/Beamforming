67f3ad296756f36c752d9afd730d620b282790c3
Chris Tousignant <ctous@vt.edu>
2021-03-09 14:47:08 -0500

    Null Widening UCA, fix ULA and UCA to work for 2 antennas

--- test/uca_beamforming.m
+++ test/uca_beamforming.m
@@ -7,8 +7,8 @@ close all
 N = 8;                % Number of Elements
 azAlly = 50;          % Target Azimuth in degrees
 elAlly = 0;
-azEnemy = [-50 -60];  % Null Azimuths
-elEnemy = [30 60];
+azEnemy = [-50 -30];  % Null Azimuths
+elEnemy = [0 0];
 fc = 22e6;            % Carrier frequency
 nullSpacing = 5;      % TODO: Null spacing in degrees
 
@@ -16,6 +16,8 @@ nullSpacing = 5;      % TODO: Null spacing in degrees
 c = physconst('LightSpeed');    % Speed of Light
 lambda = c/fc;                  % Wavelength
 diameter = lambda;              % Diameter of Array
+reservedDOF = [1 1 1 2 2 2];    % Reserved Degrees of Freedom based on N [3 4 ... 8]
+
 
 %% Globally Scoped Defines
 beams = [];             % Combined list of null and peak directions
@@ -30,11 +32,22 @@ antenna = monopoleRadial('Height',lambda/4,'RadialLength',lambda/4,'Width',.1,'R
 array = phased.UCA('NumElements',N,'Element', antenna, 'Radius', lambda/2);
 
 %% Null Widening/Placement
-% We need to do something here to select null locations
+% Determine number of available nulls to place, leaving some free
+availableNulls = N - 1 - reservedDOF(N-2);
+requiredNulls = length(azEnemy);
+if availableNulls < requiredNulls
+    error("Error: With %i antennas, only %i nulls available. Requested %i nulls", N, availableNulls, requiredNulls);
+end
+
+% Determine how many nulls to assign to each requested enemy azimuth
+nullsPerEnemyAzimuth = availableNulls / requiredNulls;
+
+% Place all enemy nulls
+wideEnemyNulls = placeNulls(azEnemy, elEnemy, nullsPerEnemyAzimuth, nullSpacing);
 
 %% Antenna Weight Calculations through LS-Minimization
 % Combine the lists of beam azimuths
-beams = [azEnemy azAlly];
+beams = [wideEnemyNulls(1,:) azAlly];
 
 % Determine the desired beamforming response
 [~,l] = min(abs(beams-azAlly));
@@ -43,7 +56,7 @@ B(l)=1/sqrt(2);
 
 % Retrieve Steering Vectors
 d = steervec(getElementPosition(array)/lambda, [azAlly; elAlly]);
-V = steervec(getElementPosition(array)/lambda, [azEnemy; elEnemy]);
+V = steervec(getElementPosition(array)/lambda, wideEnemyNulls);
 G = [V d]';
 
 % LS Minimization
@@ -62,7 +75,7 @@ cvx_begin quiet
         % Honestly not really sure what this one does... lol
         w'*w <= 1
         % Gain of 0 in the null directions
-        w'*V == zeros(1, length(azEnemy))
+        w'*V == zeros(1, length(wideEnemyNulls))
 cvx_end
 
 %% Plotting
@@ -72,33 +85,8 @@ pattern(array,fc,-180:.5:180,0,'PropagationSpeed',c,'Type','powerdb',...
     'CoordinateSystem','rectangular','Weights',w);
 xlim([-180 180]);
 hold on; legend off;
-for i = 1:length(azEnemy)
-    plot([azEnemy(i) azEnemy(i)],[-500 100],'r--','LineWidth',1)
-end
-for z = 1:length(azAlly)
-    plot([azAlly(z) azAlly(z)],[-500 100],'g--','LineWidth',1)
-end
-hold off;
-figure
-pattern(array,fc,-180:.5:180,30,'PropagationSpeed',c,'Type','powerdb',...
-    'CoordinateSystem','rectangular','Weights',w);
-xlim([-180 180]);
-hold on; legend off;
-for i = 1:length(azEnemy)
-    plot([azEnemy(i) azEnemy(i)],[-500 100],'r--','LineWidth',1)
-end
-for z = 1:length(azAlly)
-    plot([azAlly(z) azAlly(z)],[-500 100],'g--','LineWidth',1)
-end
-hold off;
-
-figure
-pattern(array,fc,-180:.5:180,60,'PropagationSpeed',c,'Type','powerdb',...
-    'CoordinateSystem','rectangular','Weights',w);
-xlim([-180 180]);
-hold on; legend off;
-for i = 1:length(azEnemy)
-    plot([azEnemy(i) azEnemy(i)],[-500 100],'r--','LineWidth',1)
+for i = 1:length(wideEnemyNulls)
+    plot([wideEnemyNulls(1,i) wideEnemyNulls(1,i)],[-500 100],'r--','LineWidth',1)
 end
 for z = 1:length(azAlly)
     plot([azAlly(z) azAlly(z)],[-500 100],'g--','LineWidth',1)
